01c4a5e7d46b0b9893bfc26c87e059aa9e9bd26c
SUISHIP <157257966+SUISHIP@users.noreply.github.com>
2024-08-10 20:24:11 +0900

    Create problems.js

---
+++ problems.js
@@ -0,0 +1,802 @@
+   const problems = [
+  {
+         question: "love at first sight/一目ぼれ",
+         answer: "sight",
+         },
+{
+         question: "a taste of lemon/レモンの味",
+         answer: "taste",
+         },
+{
+         question: "a wide range of information/広範囲の情報",
+         answer: "range",
+         },
+{
+         question: "make an appointment with the doctor/医者に予約する",
+         answer: "appointment",
+         },
+{
+         question: "a doctor and a patient/医者と患者",
+         answer: "patient",
+         },
+{
+         question: "a business project/事業計画",
+         answer: "project",
+         },
+{
+         question: "Would you do me a favor/頼みを聞いてもらえませんか",
+         answer: "favor",
+         },
+{
+         question: "differ in appearance/外見が違う",
+         answer: "appearance",
+         },
+{
+         question: "run the risk of losing money/お金を失う危険を冒す",
+         answer: "risk",
+         },
+{
+         question: "costs and benefits of the business/仕事のコストと利益",
+         answer: "benefits",
+         },
+{
+         question: "residents of New York/ニューヨークの市民",
+         answer: "residents",
+         },
+{
+         question: "their relatives and friends/彼らの親戚と友達",
+         answer: "relatives,",
+         },
+{
+         question: "a mountain region/山岳地方",
+         answer: "region",
+         },
+{
+         question: "unique characteristics/ユニークな特徴",
+         answer: "characteristics",
+         },
+{
+         question: "feel a sharp pain/鋭い痛みを感じる",
+         answer: "pain",
+         },
+{
+         question: "a pair of identical twins/一組の一卵双生児",
+         answer: "twins",
+         },
+{
+         question: "on special occasions/特別な場合に",
+         answer: "occasions",
+         },
+{
+         question: "the principle of free trade/自由貿易の原則",
+         answer: "principle",
+         },
+{
+         question: "the history department/歴史科学",
+         answer: "department",
+         },
+{
+         question: "It is my duty to help you./君を助けるのが私の義務だ",
+         answer: "duty",
+         },
+{
+         question: "the scene of the accident/事故の現場",
+         answer: "scene",
+         },
+{
+         question: "avoid traffic jams/交通渋滞を避ける",
+         answer: "jams",
+         },
+{
+         question: "the spirit of fair play/フェアプレーの精神",
+         answer: "spirit",
+         },
+{
+         question: "the medium of communication/コミュニケーションの手段",
+         answer: "medium",
+         },
+{
+         question: "mass production/大量生産",
+         answer: "mass",
+         },
+{
+         question: "gather a large audience/大勢の観客を集める",
+         answer: "audience",
+         },
+{
+         question: "the most important element/最も重要な要素",
+         answer: "element",
+         },
+{
+         question: "global climate change/地球規模の気候変動",
+         answer: "climate",
+         },
+{
+         question: "the French revolution/フランス革命",
+         answer: "revolution",
+         },
+{
+         question: "the first quarter of this century/今世紀の最初の4分の1",
+         answer: "quarter",
+         },
+{
+         question: "a room with little furniture/家具の少ない部屋",
+         answer: "furniture",
+         },
+{
+         question: "the human brain/人間の脳",
+         answer: "brain",
+         },
+{
+         question: "co2 in the earth's atmosphere/地球の大気中の二酸化炭素",
+         answer: "co2, atmosphere",
+         },
+{
+         question: "private property/私有財産",
+         answer: "property",
+         },
+{
+         question: "a reward for hard work/努力の報酬",
+         answer: "reward,
+         },
+{
+         question: "national security/国家の安全保障",
+         answer: "security",
+         },
+{
+         question: "give a cry of delight/喜びの声をあげる",
+         answer: "delight",
+         },
+{
+         question: "a deserted road in the desert/砂漠の人影のない道",
+         answer: "desert",
+         },
+{
+         question: "people from different backgrounds/経歴の違う人々",
+         answer: "backgrounds",
+         },
+{
+         question: "a trend toward fewer children/少子化の傾向",
+         answer: "trend",
+         },
+{
+         question: "get 20% of the vote/20%の票を得る",
+         answer: "vote",
+         },
+{
+         question: "a negative impact on the environment/環境に対する悪い影響",
+         answer: "impact",
+         },
+{
+         question: "educational institutions/教育機関",
+         answer: "institutions",
+         },
+{
+         question: "social interaction with others/他人との社会的交流",
+         answer: "interaction",
+         },
+{
+         question: "an alternative to oil/石油の代わりになるもの",
+         answer: "alternative",
+         },
+{
+         question: "do no harm to children/子供に害を与えない",
+         answer: "harm",
+         },
+{
+         question: "go to a travel agency/旅行代理店に行く",
+         answer: "agency",
+         },
+{
+         question: "man's great capacity to learn/すばらしい人間の学習能力",
+         answer: "capacity",
+         },
+{
+         question: "the Italian foreign minister/イタリアの外務大臣",
+         answer: "minister",
+         },
+{
+         question: "a hospital volunteer/病院で働くボランティア",
+         answer: "volunteer",
+         },
+{
+         question: "have access to the Internet/インターネットを利用できる",
+         answer: "access",
+         },
+{
+         question: "large quantities of data/膨大な量のデータ",
+         answer: "quantities",
+         },
+{
+         question: "a branch of science/科学の一分野",
+         answer: "branch",
+         },
+{
+         question: "a common language/共通の言語",
+         answer: "common",
+         },
+{
+         question: "a rough sketch/大雑把なスケッチ",
+         answer: "rough",
+         },
+{
+         question: "He is likely to win./彼が勝つ可能性が高い",
+         answer: "likely",
+         },
+{
+         question: "serious social problems/深刻な社会問題",
+         answer: "serious",
+         },
+{
+         question: "a particular character/特有の性質",
+         answer: "particular",
+         },
+{
+         question: "information available to everyone/みんなが利用できる情報",
+         answer: "available",
+         },
+{
+         question: "bilingual children/二言語使用の子供たち",
+         answer: "bilingual",
+         },
+{
+         question: "I am ready to start./出発の用意ができている",
+         answer: "ready",
+         },
+{
+         question: "the correct answer/正しい答え",
+         answer: "correct",
+         },
+{
+         question: "be familiar with Japanese culture/日本の文化にくわしい",
+         answer: "familiar",
+         },
+{
+         question: "physical beauty/肉体美",
+         answer: "physical",
+         },
+{
+         question: "The book is worth reading./その本は読む価値がある",
+         answer: "worth",
+         },
+{
+         question: "be involved in the accident/事故に巻き込まれている",
+         answer: "involved",
+         },
+{
+         question: "I had a fantastic time/私は素晴らしい時を過ごした",
+         answer: "fantastic",
+         },
+{
+         question: "her private life/彼女の私生活",
+         answer: "private",
+         },
+{
+         question: "an obvious mistake/明白なまちがい",
+         answer: "obvious",
+         },
+{
+         question: "a native language/母語",
+         answer: "native",
+         },
+{
+         question: "a complex system/複雑なシステム",
+         answer: "complex",
+         },
+{
+         question: "I am willing to help her./喜んで彼女を助けるつもりだ",
+         answer: "willing",
+         },
+{
+         question: "the current international situation/今日の国際状況",
+         answer: "current",
+         },
+{
+         question: "male workers/男性の労働者",
+         answer: "male",
+         },
+{
+         question: "the proper use of words/言葉の適切な使い方",
+         answer: "proper",
+         },
+{
+         question: "He is capable of doing the job./彼はその仕事をする能力がある",
+         answer: "capable",
+         },
+{
+         question: "He is independent of his parents./彼は親から独立している。",
+         answer: "independent",
+         },
+{
+         question: "positive thinking/積極的な考え方",
+         answer: "positive",
+         },
+{
+         question: "a pleasant experience/楽しい経験",
+         answer: "pleasant",
+         },
+{
+         question: "a significant difference/重大な違い",
+         answer: "significant",
+         },
+{
+         question: "the former President/前大統領",
+         answer: "former",
+         },
+{
+         question: "a chemical reaction/化学反応",
+         answer: "chemical",
+         },
+{
+         question: "be upset by the accident/事故で動揺している",
+         answer: "upset",
+         },
+{
+         question: "from the previous year/前の年から",
+         answer: "previous",
+         },
+{
+         question: "keep calm/冷静でいる",
+         answer: "calm",
+         },
+{
+         question: "a specific individual/特定の個人",
+         answer: "specific",
+         },
+{
+         question: "health-conscious Americans/健康を意識するアメリカ人",
+         answer: "conscious",
+         },
+{
+         question: "be superior to others/他の人よりすぐれている",
+         answer: "superior",
+         },
+{
+         question: "an efficient use of energy/効率のよいエネルギーの使い方",
+         answer: "efficient",
+         },
+{
+         question: "fundamental human rights/基本的人権",
+         answer: "fundamental",
+         },
+{
+         question: "a narrow street/狭い道",
+         answer: "narrow",
+         },
+{
+         question: "a reasonable explanation/理にかなった説明",
+         answer: "reasonable",
+         },
+{
+         question: "feel nervous about the future/将来のことで不安になる",
+         answer: "nervous",
+         },
+{
+         question: "The brothers look alike./その兄弟は似ている",
+         answer: "alike",
+         },
+{
+         question: "domestic violence/家庭内暴力",
+         answer: "domestic",
+         },
+{
+         question: "a negative answer/否定的な答え",
+         answer: "negative",
+         },
+{
+         question: "make a moral judgment/道徳的な判断をする",
+         answer: "moral",
+         },
+{
+         question: "be eager to study in the US/アメリカ留学を熱望する",
+         answer: "eager",
+         },
+{
+         question: "the brain's remarkable ability/脳のすばらしい能力",
+         answer: "remarkable",
+         },
+{
+         question: "drive away evil spirits/悪い霊を追い払う",
+         answer: "evil",
+         },
+{
+  question: "stay awake all night/夜通し目が覚めている",
+  answer: "awake"
+         },
+{
+  question: "his aged parents/彼の年老いた父母",
+  answer: "aged"
+         },
+{
+  question: "I am anxious about your health./君の健康が心配だ",
+  answer: "anxious"
+         },
+{
+  question: "a tough boxer/たくましいボクサー",
+  answer: "tough"
+         },
+{
+  question: "nuclear energy/原子力エネルギー",
+  answer: "nuclear"
+         },
+{
+  question: "the British legal system/イギリスの法律の制度",
+  answer: "legal"
+         },
+{
+  question: "be curious about everything/何にでも好奇心を持つ",
+  answer: "curious"
+         },
+{
+  question: "civil rights/市民権",
+  answer: "civil"
+         },
+{
+  question: "according to a recent study/最近の研究によると",
+  answer: "recent"
+         },
+{
+  question: "a senior member of the club/クラブの先輩の部員",
+  answer: "senior"
+         },
+{
+  question: "Soon afterward, he left/その後すぐ彼は去った",
+  answer: "afterward"
+         },
+{
+  question: "nearly 30 years ago/30年近く前に",
+  answer: "nearly"
+         },
+{
+  question: "The car is small and therefore cheap./その車は小さい。それゆえ、安い。",
+  answer: "therefore"
+         },
+{
+  question: "at exactly the same time/ぴったり同時に",
+  answer: "exactly"
+         },
+{
+  question: "He will possibly come./ひょっとすると彼は来るかもしれない",
+  answer: "possibly"
+         },
+{
+  question: "contrary to expectations/予想に反して",
+  answer: "contrary"
+         },
+{
+  question: "I occasionally go to the theater./私はたまに劇場に行く",
+  answer: "occasionally"
+         },
+{
+  question: "Somehow I feel lonely./なぜか寂しい",
+  answer: "somehow"
+         },
+{
+  question: "I seldom see him./彼に会うことはめったにない",
+  answer: "seldom"
+         },
+{
+  question: "This is smaller and thus cheaper./この方が小さく、したがって安い",
+  answer: "thus"
+         },
+{
+  question: "people throughout the world/世界中の人々",
+  answer: "throughout"
+         },
+{
+  question: "Unlike my wife, I get up early./妻と違って私は早起きだ",
+  answer: "unlike"
+         },
+{
+  question: "Besides being rich, he is kind./彼は金持ちの上にやさしい",
+  answer: "besides"
+         },
+{
+  question: "It’s beyond my understanding./私の理解をこえている",
+  answer: "beyond"
+         },
+{
+  question: "within a mile of the station/駅から１マイル以内で",
+  answer: "within"
+         },
+{
+  question: "have neither time nor money/時間もお金もない",
+  answer: "neither"
+         },
+{
+  question: "I’ll leave tomorrow unless it rains./明日雨が降らない限り出発する",
+  answer: "unless"
+         },
+{
+  question: "work every day except Sunday/日曜以外毎日働く",
+  answer: "except"
+         },
+{
+  question: "You ought to see a doctor/君は医者に診てもらうべきだ",
+  answer: "ought"
+         },
+{
+  question: "in spite of difficulties/困難にもかかわらず",
+  answer: "in spite of"
+         },
+{
+  question: "I don’t know whether it is true or not./本当かどうかわからない",
+  answer: "whether"
+         },
+{
+    question: "explain why he was late/彼がなぜ遅れたかを説明する",
+    answer: "explain",
+         },
+{
+    question: "accept the truth as it is/ありのまま真実を受け入れる",
+    answer: "accept",
+         },
+{
+    question: "produce enough food/十分な食料を生産する",
+    answer: "produce",
+         },
+{
+    question: "Does God really exist?/神は本当に存在するのか",
+    answer: "exist",
+         },
+{
+    question: "express my feelings/私の気持ちを表現する",
+    answer: "express",
+         },
+{
+    question: "add some milk to the soup/スープにミルクを加える",
+    answer: "add",
+         },
+{
+    question: "avoid making mistakes/まちがいを犯すのを避ける",
+    answer: "avoid",
+         },
+{
+    question: "marry Mary/メアリと結婚する",
+    answer: "marry",
+         },
+{
+    question: "protect children from danger/危険から子どもたちを守る",
+    answer: "protect",
+         },
+{
+    question: "Alcohol affects the brain./アルコールは脳に影響する",
+    answer: "affects",
+         },
+{
+    question: "determine your future/君の未来を決定する",
+    answer: "determine",
+         },
+{
+    question: "solve the problem/問題を解決する",
+    answer: "solve",
+         },
+{
+    question: "Vegetables contain a lot of water./野菜はたくさんの水を含んでいる",
+    answer: "contain",
+         },
+{
+    question: "discuss the problem with him/彼とその問題を議論する",
+    answer: "discuss",
+         },
+{
+    question: "ignore the doctor's advice/医者の忠告を無視する",
+    answer: "ignore",
+         },
+{
+    question: "guess how old she is/彼女の年を推測する",
+    answer: "guess",
+         },
+{
+    question: "exchange yen for dollars/円をドルに交換する",
+    answer: "exchange",
+         },
+{
+    question: "satisfy the needs of students/学生の要求を満たす",
+    answer: "satisfy",
+         },
+{
+    question: "complain about the noise/騒音のことで苦情を言う",
+    answer: "complain",
+         },
+{
+    question: "finally achieve the goal/ついに目標を達成する",
+    answer: "achieve",
+         },
+{
+    question: "enable people to live longer/人々の長寿を可能にする",
+    answer: "enable",
+         },
+{
+    question: "intend to live in America/アメリカに住むつもりだ",
+    answer: "intend",
+         },
+{
+    question: "obtain information about him/彼に関する情報を得る",
+    answer: "obtain",
+         },
+{
+    question: "divide the cake into six pieces/ケーキを6個に分割する",
+    answer: "divide",
+         },
+{
+    question: "The noise annoys me./その音が私をいらだたせる",
+    answer: "annoys",
+         },
+{
+    question: "My opinion differs from hers./私の考えは彼女と異なる",
+    answer: "differs",
+         },
+{
+    question: "how to educate children/子供を教育する方法",
+    answer: "educate",
+         },
+{
+    question: "borrow a book from a friend/友達から本を借りる",
+    answer: "borrow",
+         },
+{
+    question: "invent a time machine/タイムマシンを発明する",
+    answer: "invent",
+         },
+{
+    question: "promote economic growth/経済成長を促進する",
+    answer: "promote",
+         },
+{
+    question: "advise him to eat vegetables/野菜を食べるよう彼に忠告する",
+    answer: "advise",
+         },
+{
+    question: "retire from work at sixty/60で仕事を辞める",
+    answer: "retire",
+         },
+{
+    question: "permit him to go out/彼に外出することを許す",
+    answer: "permit",
+         },
+{
+    question: "recommend this book to you/あなたにこの本を勧める",
+    answer: "recommend",
+         },
+{
+    question: "apologize to him for being late/遅れたことを彼に謝る",
+    answer: "apologize",
+         },
+{
+    question: "inform him of his son's success/息子の成功を彼に知らせる",
+    answer: "inform",
+         },
+{
+    question: "oppose their marriage/彼らに結婚に反対する",
+    answer: "oppose",
+         },
+{
+    question: "trust an old friend/古い友達を信用する",
+    answer: "trust",
+         },
+{
+    question: "select the best answer/最良の答えを選ぶ",
+    answer: "select",
+         },
+{
+    question: "praise him for his work/仕事のことで彼をほめる",
+    answer: "praise",
+         },
+{
+    question: "how to handle problems/どう問題に対処するべきか",
+    answer: "handle",
+         },
+{
+    question: "propose a new way/新しいやり方を提案する",
+    answer: "propose",
+         },
+{
+    question: "breathe fresh air/新鮮な空気を呼吸する",
+    answer: "breathe",
+         },
+{
+    question: "criticize him for being late/遅刻したことで彼を非難する",
+    answer: "criticize",
+         },
+{
+    question: "overcome difficulties/困難に打ち勝つ",
+    answer: "overcome",
+         },
+{
+    question: "possess great power/大きな力を持っている",
+    answer: "possess",
+         },
+{
+    question: "predict the future/未来を予言する",
+    answer: "predict",
+         },
+{
+    question: "publish a book/本を出版する",
+    answer: "publish",
+         },
+{
+    question: "leaves floating on the river/川面に浮かぶ木の葉",
+    answer: "floating",
+         },
+{
+    question: "recall the good old days/古き良き時代を思い出す",
+    answer: "recall",
+         },
+{
+    question: "explore the Amazon River/アマゾン川を探検する",
+    answer: "explore",
+         },
+{
+    question: "pretend to be asleep/眠っているふりをする",
+    answer: "pretend",
+         },
+{
+    question: "absorb a lot of water/大量の水を吸収する",
+    answer: "absorb",
+         },
+{
+    question: "He resembles his father./彼は父親に似ている。",
+    answer: "resembles",
+         },
+{
+    question: "tear the letter to pieces/ずたずたに手紙を引き裂く",
+    answer: "tear",
+         },
+{
+    question: "consume a lot of energy/多量のエネルギーを消費する",
+    answer: "consume",
+         },
+{
+    question: "compete with him for the gold medal/金メダルを目指して彼と競争する",
+    answer: "compete",
+         },
+{
+    question: "quit smoking/タバコをやめる",
+    answer: "quit",
+         },
+{
+    question: "announce a new plan/新しい計画を発表する",
+    answer: "announce",
+         },
+{
+    question: "react quickly to light/光にすばやく反応する",
+    answer: "react",
+         },
+{
+    question: "wander around the streets/街を歩き回る",
+    answer: "wander",
+         },
+{
+    question: "Don't text while driving/運転中にメールを送るな",
+    answer: "text",
+         },
+{
+    question: "generate electricity/電力を生み出す",
+    answer: "generate",
+         },
+{
+    question: "score 10 goals/10点を取る",
+    answer: "score",
+         },
+{
+    question: "the Japanese government/日本政府",
+    answer: "government",
+         },
+{
+    question: "have little knowledge of English/英語の知識がほとんどない",
+    answer: "have",
+         },
+{
+    question: "the Asian nations/アジアの諸国",
+    answer: "nations",
+         },
+{
+    question: "make an effort to help him/彼を助けようと努力する",
+    answer: "make",
+         },
+{
+    question: "the Cold War period/冷戦時代",
+    answer: "period",
+         },
+  ];
